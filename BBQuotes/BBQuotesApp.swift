c5880fc1aa5194a9bb0d5e82fb312ea22dd1bc3a
den330 <wo131485508@qq.com>
2025-02-22 10:00:18 -0500

    Initial Commit

---
+++ BBQuotes/BBQuotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  BBQuotesApp.swift
+//  BBQuotes
+//
+//  Created by yaxin on 2025-02-22.
+//
+
+import SwiftUI
+
+@main
+struct BBQuotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
